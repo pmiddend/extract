e89f34fb1865ab637e62b655ef36368af6e5dc77
phillemann <pmidden@gmx.net>
2009-12-30 18:43:28 +0100

    added my first type! :)

---
+++ mime_type.hpp
@@ -0,0 +1,13 @@
+#ifndef EXTRACT_MIME_TYPE_HPP_HPP_INCLUDED
+#define EXTRACT_MIME_TYPE_HPP_HPP_INCLUDED
+
+#include <fcppt/string.hpp>
+
+namespace extract
+{
+typedef
+fcppt::string
+mime_type;
+}
+
+#endif
