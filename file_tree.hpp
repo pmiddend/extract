7b4231d5e1563de515125084df6086ac719f1f13
Philipp Reh <sefi@s-e-f-i.de>
2010-01-23 13:47:57 +0100

    Update for fcppt.

--- file_tree.hpp
+++ file_tree.hpp
@@ -1,12 +1,12 @@
 #ifndef EXTRACT_FILE_TREE_HPP_HPP_INCLUDED
 #define EXTRACT_FILE_TREE_HPP_HPP_INCLUDED
 
-#include <fcppt/container/tree.hpp>
+#include <fcppt/container/tree/object.hpp>
 
 namespace extract
 {
 typedef
-fcppt::container::tree<fcppt::string>
+fcppt::container::tree::object<fcppt::string>
 file_tree;
 }
 
